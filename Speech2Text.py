516dfa4f14dd632d2a8834c62ca6912be42ea9b4
Kannan <kannangates@gmail.com>
2021-06-17 23:24:23 +0530

    touchup

--- Speech2Text.py
+++ Speech2Text.py
@@ -1,30 +1,34 @@
+from google.cloud import storage
+import wave
+from google.cloud.speech import types
+from google.cloud.speech import enums
+from google.cloud import speech
+import os
+import io
+from pydub import AudioSegment
 filepath = "audio_wav/"
 output_filepath = "Transcripts/"
-bucket_name='aud2txt'
+bucket_name = 'aud2txt'
 
-from pydub import AudioSegment
-import io
-import os
-from google.cloud import speech
-from google.cloud.speech import enums
-from google.cloud.speech import types
-import wave
-from google.cloud import storage
 
-#Converting mp3 to wav format
+# Converting mp3 to wav format
 def mp3_to_wav(audio_file_name):
     if audio_file_name.split('.')[1] == 'mp3':
         sound = AudioSegment.from_mp3(audio_file_name)
         audio_file_name = audio_file_name.split('.')[0] + '.wav'
         sound.export(audio_file_name, format="wav")
 
-#Converting stereo to mono, i.e. changing to 1 channel
+# Converting stereo to mono, i.e. changing to 1 channel
+
+
 def stereo_to_mono(audio_file_name):
     sound = AudioSegment.from_wav(audio_file_name)
     sound = sound.set_channels(1)
     sound.export(audio_file_name, format="wav")
 
-#Uploading audio file
+# Uploading audio file
+
+
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     storage_client = storage.Client()
@@ -32,7 +36,9 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob = bucket.blob(destination_blob_name)
     blob.upload_from_filename(source_file_name)
 
-#Deleting audio file
+# Deleting audio file
+
+
 def delete_blob(bucket_name, blob_name):
     """Deletes a blob from the bucket."""
     storage_client = storage.Client()
@@ -40,14 +46,15 @@ def delete_blob(bucket_name, blob_name):
     blob = bucket.blob(blob_name)
     blob.delete()
 
-#google_Transcribe
+# Google_Transcribe
+
+
 def google_transcribe(audio_file_name):
 
     file_name = filepath + audio_file_name
     mp3_to_wav(file_name)
 
     # The name of the audio file to transcribe
-
     frame_rate, channels = frame_rate_channel(file_name)
 
     if channels > 1:
@@ -66,9 +73,9 @@ def google_transcribe(audio_file_name):
     audio = types.RecognitionAudio(uri=gcs_uri)
 
     config = types.RecognitionConfig(
-    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    sample_rate_hertz=frame_rate,
-    language_code='en-US')
+        encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=frame_rate,
+        language_code='en-US')
 
     # Detects speech in the audio file
     operation = client.long_running_recognize(config, audio)
@@ -80,13 +87,15 @@ def google_transcribe(audio_file_name):
     delete_blob(bucket_name, destination_blob_name)
     return transcript
 
-def write_transcripts(transcript_filename,transcript):
-    f= open(output_filepath + transcript_filename,"w+")
+
+def write_transcripts(transcript_filename, transcript):
+    f = open(output_filepath + transcript_filename, "w+")
     f.write(transcript)
     f.close()
+
 
 if __name__ == "__main__":
     for audio_file_name in os.listdir(filepath):
         transcript = google_transcribe(audio_file_name)
         transcript_filename = audio_file_name.split('.')[0] + '.txt'
-        write_transcripts(transcript_filename,transcript)
+        write_transcripts(transcript_filename, transcript)
